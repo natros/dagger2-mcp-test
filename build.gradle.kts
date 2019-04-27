3511412edb005b75cec562e83c981e961ab664ce
Filipe Sousa <natros@gmail.com>
2019-04-27 11:29:40 +0100

    force java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,11 @@ repositories {
     mavenCentral()
 }
 
+java {
+  sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
+}
+
 dependencies {
     implementation("com.google.dagger:dagger:2.22.1")
     annotationProcessor("com.google.dagger:dagger-compiler:2.22.1")
